ac3632555121ff9d1c8160dcd559c3461daa021b
Percival289 <57201358+Percival289@users.noreply.github.com>
2021-01-21 15:31:28 -0500

    Throttle Animations

--- PFS757/PackageSources/html_ui/Pages/VCockpit/Instruments/Airliners/B757_2/EADI/B757_200_EADI.js
+++ PFS757/PackageSources/html_ui/Pages/VCockpit/Instruments/Airliners/B757_2/EADI/B757_200_EADI.js
@@ -1,66 +1,29 @@
-class B757_200_EADI extends BaseAirliners {
-
-  constructor() {
-    super();
-    this.initDuration = 7000;
-  }
-
-  get templateID() {
-    return "B757_200_EADI";
-  }
-
-  get IsGlassCockpit() {
-    return true;
-  }
-
+'use strict';
+class B757_200_EADI extends BaseInstrument {
+  // match id="MY_INSTRUMENT" in html
+  get templateID() { return 'MY_INSTRUMENT'; }
+  // true if you want people to be able to click and type on the instrument
+  get isInteractive() { return false; }
+  // i'm not sure what this actually changes but set it to true if its a glass cockpit instrument i guess
+  get isGlassCockpit() { return true; }
+ 
   connectedCallback() {
-    super.connectedCallback();
-    this.pageGroups = [
-      new NavSystemPageGroup("Main", this, [
-          new B757_200_EADI_MainPage()
-      ]),
-    ];
-      this.maxUpdateBudget = 12;
+    // code here runs when html is mounted
   }
-  disconnectedCallback() {
-    window.console.log("A320 Neo PFD - destroyed");
-    super.disconnectedCallback();
+
+  // runs every frame
+  Update() {
+    super.Update();
+    if (this.CanUpdate()) {
+      // code here runs based on the "update frequency" setting in graphics
+    }
+  }
+
+  // receive H events here
+  onInteractionEvent(event) {
+    const eventName = String(event);
+    // ...
   }
 }
-class B757_200_EADI_MainElement extends NavSystemElement {
-    init(root) {
-    }
-    onEnter() {
-    }
-    onUpdate(_deltaTime) {
-    }
-    onExit() {
-    }
-    onEvent(_event) {
-    }
-}
-class B757_200_EADI_MainPage extends NavSystemPage {
-  constructor() {
-        super("Main", "Mainframe", new B757_200_EADI_MainElement());
-        this.showILS = false;
-        this.attitude = new B757_200_EADI_Attitude();
-        this.vSpeed = new B757_200_EADI_VSpeed();
-        this.airspeed = new B757_200_EADI_Airspeed();
-        this.altimeter = new B757_200_EADI_Altimeter();
-        this.compass = new B757_200_EADI_Compass();
-        this.navStatus = new B757_200_EADI_NavStatus();
-        this.ils = new B757_200_EADI_ILS();
-        this.element = new NavSystemElementGroup([
-            this.attitude,
-            this.vSpeed,
-            this.airspeed,
-            this.altimeter,
-            this.compass,
-            this.navStatus,
-            this.ils
-        ]);
-    }
-    init() {
-      super.init();
-      
-    }
+
+registerInstrument('my-instrument-element', B757_200_EADI);
